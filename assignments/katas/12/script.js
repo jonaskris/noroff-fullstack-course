4ed1d6ee3b532300bf2e4898350c51042ffb0485
Jonas Kristoffersen <jo_nas@live.no>
2022-02-10 16:24:51 +0100

    feat: implemented kata 12

--- assignments/katas/12/script.js
+++ assignments/katas/12/script.js
@@ -2,105 +2,79 @@ const inputTextFirstElement = document.getElementById("input_text_first");
 const inputTextSecondElement = document.getElementById("input_text_second"); 
 const outputTextElement = document.getElementById("output_text");
 
-const regexMatchLowercaseAlphabet = new RegExp(/[a-z]/);
-
-function filterMatchLowercaseAlphabet(string) {
-    return regexMatchLowercaseAlphabet.test(string);
-}
-
-function numArrayDifference(numArrayLeft, numArrayRight) {
-    return numArrayLeft.map((numFirst, index) => {
-        let numSecond = numArrayRight[index];
-        return numFirst - numSecond;
-    });
-}
-
-function charCount(stringArray) {
+/*
+    Returns an array of integers where 
+    index 0-26 are the number of occurences of characters a-z in string.
+*/
+function charCount(string) {
     let charCountArray = Array(26).fill(0);
     
-    stringArray.forEach((char) => {
-        let charCountArrayIndex = char.charCodeAt(0) - 97;
-        charCountArray[charCountArrayIndex]++;
-    });
+    for(const c of string)
+        charCountArray[c.charCodeAt(0) - 97]++;
 
     return charCountArray;
 }
 
-function numArraySumReduce(total, current) {
-    return total + current;
-}
-
-function findRangesContainingCountElements(array, element, count) {
-    let occurences = [];
-
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === element) {
-            occurences.push(i);
-        }
-    }
-
-    let ranges = [];
-    for(let i = 0; i < occurences.length - count + 1; i++){
-        ranges.push([occurences[i], occurences[i + count - 1]]);
-    }
-
-    return ranges;
+/*
+    Returns true if character is of range a-z.
+*/
+const regexMatchLowercaseAlphabet = new RegExp(/[a-z]/);
+function filterMatchLowercaseAlphabet(string) {
+    return regexMatchLowercaseAlphabet.test(string);
 }
 
 /*
-    1. Instantiate array of numbers = 0, N = 26. Index of 'A' = 0, 'Z' = 25
+    Checks every continuous substring X in firstString of length secondString.length.
+
+    If the number of each character in substring equals the number of each character in secondString,
+    it is a valid anagram and gets added to array of found anagrams.
+
+    Returns all found anagrams.
 */
 function findAnagram(firstString, secondString) {
-    // Filter out any character not in range a-z
-    let firstArray = Array.from(firstString.toLowerCase())
-        .filter(filterMatchLowercaseAlphabet);
-    firstString = firstArray.join("");
+    /* 
+        Convert input strings to lowercase and filter out characters not in range a-z.
+    */
+    firstString = Array.from(firstString.toLowerCase())
+        .filter(filterMatchLowercaseAlphabet).join("");
 
-    let secondArray = Array.from(secondString.toLowerCase())
-        .filter(filterMatchLowercaseAlphabet);
-    secondString = secondArray.join("");
+    secondString = Array.from(secondString.toLowerCase())
+        .filter(filterMatchLowercaseAlphabet).join("");
 
-    // Find character occurence second string
-    let secondCharCount = charCount(secondArray).map((count, index) => [String.fromCharCode(index + 97), count])
-        .filter(([char, count]) => count > 0);
+    /* 
+        Find charCount of secondString.
+    */
+    let secondCharCount = charCount(secondString);
 
-    console.log("characterCounts: ", secondCharCount);
+    /* 
+        Iterates over all continuous substrings in firstString with length of secondString.
 
-    // Find every range which has exactly count of char
-    let characterRanges = Array(26).fill([]);
-    for(let i = 0; i < secondCharCount.length; i++) {
-        characterRanges[secondCharCount[i][0].charCodeAt(0) - 97] = findRangesContainingCountElements(firstArray, secondCharCount[i][0], secondCharCount[i][1]);
+        1. Find charCount of substring.
+        2. Check element-wise equality of substringCharCount and secondCharCount.
+            2.1 All true: Add substring index range to foundAnagrams.
+            2.2 Any false: Check next substring.
+    */
+    let foundAnagrams = [];
+    for(let i = 0; i < firstString.length - secondString.length + 1; i++) {
+        let substringCharCount = charCount(firstString.slice(i, i + secondString.length));
+        
+        let substringIsAnagram = secondCharCount
+            .map((charCount, index) => charCount === substringCharCount[index])
+            .reduce((acc, val) => acc && val, true);
+
+        if(substringIsAnagram)
+            foundAnagrams.push([i, i + secondString.length]);
     }
 
-    console.log("characterRanges: ", characterRanges);
-
-    let rangeContribution = Array(firstArray.length).fill(0);
-    for(let i = 0; i < characterRanges.length; i++) {
-        for(let j = 0; j < characterRanges[i].length; j++) {
-            for(let k = characterRanges[i][j][0]; k <= characterRanges[i][j][1]; k++) {
-                rangeContribution[k] = rangeContribution[k]*26 + 1;
-            }
-        }
-    }
-
-    console.log("rangeContribution: \n", rangeContribution.join(" "));
-    console.log("firstArray: \n", firstArray.join(" "));
-    
-
-    //let ranges = findRangesContainingElements(firstArray, sortedByCount);
-    //return ranges.map(([first, last]) => firstArray.slice(first, last+1).join(""));
-    return "huh";
+    /* 
+        Return firstString substrings present in foundAnagrams.
+    */
+    return foundAnagrams.map(([firstIndex, lastIndex]) => firstString.slice(firstIndex, lastIndex));
 }
 
-/*
-    An old west action hero actor | Clint Eastwood
-    o: 2-13, 13-18, 18-22
-    n: 1-1, 14-14
-    t: 8-11, 11-21
-*/
 function findAnagramButtonHandler() {
     var inputTextFirst = inputTextFirstElement.value;
     var inputTextSecond = inputTextSecondElement.value;
 
-    outputTextElement.innerText = findAnagram(inputTextFirst, inputTextSecond);;//findAnagram(inputTextFirst, inputTextSecond).join(", ");
+    outputTextElement.innerText = "Found anagrams: \n" + findAnagram(inputTextFirst, inputTextSecond).join("\n");
 }
